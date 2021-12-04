57d01e1ea21ebe7dbf2bd31ebd2d5f95b55df1b4
oldboyprq <67952647+oldboyprq@users.noreply.github.com>
2021-12-04 13:51:31 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,3 +1,4 @@
+
 import time
 import requests
 import json
@@ -6,13 +7,13 @@ import json
 def run():
     print("new round")
 #     payload = json.dumps({"ref": "main"})
-    key = “iYCKxRNPAeR8”
-    token = "ghp_{}".fortmat(key)
+    key = "iYCKxRNPAeR8"
+    token = "ghp_{}".format(key)
     header = {'Authorization': 'token {}1t9ZBL9H4v1FfvPXA54Ujfh7'.format(token),
               "Accept": "application/vnd.github.v3+json"}
 #     name_list = ['files_rename','E5api','wzry-skins']
     url_list = [r'https://api.github.com/repos/oldboyprq/E5api/actions/workflows/Task.yml/dispatches',]
-#                 r'https://api.github.com/repos/oldboyprq/files_rename/actions/workflows/Task.yml/dispatches',   
+#                 r'https://api.github.com/repos/oldboyprq/files_rename/actions/workflows/Task.yml/dispatches',
 #                 r'https://api.github.com/repos/oldboyprq/wzry-skins/actions/workflows/Task.yml/dispatches'
     for url in url_list:
 #         if url_list.index(url) > 0:
